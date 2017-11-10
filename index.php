0798c6fb1d881a6230d9266dc3446f31196b7584
Rasheed Abdullah <rasheedabdullah967@gmail.com>
2017-11-10 17:21:30 +0300

    update 2 index.php

--- index.php
+++ index.php
@@ -1,3 +1,12 @@
 <?php
-echo "Hello World";
+    echo "Hello World";
+
+    if(isset($_REQUEST['hub_challenge'])){
+        $challenge=$_REQUEST['hub_challenge'];
+        $token=$_REQUEST['hub_verify_token'];
+    }
+
+    if($token=="myCustomToken123"){
+        echo $challenge;
+    }
 ?>
